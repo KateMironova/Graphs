b252eae7884f53b68c119147cb5d7df9cecd3ff6
Mironova Ekaterina <ekmironova123@gmail.com>
2017-10-11 23:58:52 +0300

    Save

--- Graphs/Graph_AdjList.cs
+++ Graphs/Graph_AdjList.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Graphs
 {
-    public class Graph_AdjList
+    public class Graph_AdjgrList : IGraph
     {
         public class Vertex
         {
@@ -28,7 +28,67 @@ namespace Graphs
                 this.distance = distance;
             }
         }
-        private List<KeyValuePair<Vertex, List<KeyValuePair<Vertex, int>>>> v = new List<KeyValuePair<Vertex, List<KeyValuePair<Vertex, int>>>>();
 
+        private Dictionary<Vertex, List<KeyValuePair<Vertex, Edge>>> grList = new Dictionary<Vertex, List<KeyValuePair<Vertex, Edge>>>();
+
+        public void AddVertex(string name)
+        {
+            if (grList.Keys.FirstOrDefault((x) => x.name == name) == null)
+            {
+                grList.Add(new Vertex(name), new List<KeyValuePair<Vertex, Edge>>());
+            }
+        }
+
+        public void AddEdge(string from, string to, int val)
+        {
+            Vertex vFrom = grList.Keys.First((x) => x.name == from);
+            Vertex vTo = grList.Keys.FirstOrDefault((x) => x.name == to);
+            
+            if (vFrom == null)
+            {
+                AddVertex(from);
+                vFrom = grList.Keys.First((x) => x.name == from);
+            }
+            if (vTo == null)
+            {
+                AddVertex(to);
+                vTo = grList.Keys.FirstOrDefault((x) => x.name == to);
+            }
+            grList[vFrom].Add(new KeyValuePair<Vertex, Edge>(vTo, new Edge(vFrom, vTo, val)));
+
+        }
+        public void DelVertex(string name)
+        {
+            Vertex vDel = grList.Keys.First((x) => x.name == name);
+            grList.Remove(vDel);
+        }
+
+        public int DelEdge(string from, string to)
+        {
+
+            throw new NotImplementedException();
+        }
+
+        public void Print()
+        {
+            foreach (var keyValuePair in grList)
+            {
+                Console.WriteLine("From {0}:", keyValuePair.Key.name);
+                foreach (var item in keyValuePair.Value)
+                {
+                    Console.WriteLine("\t to {0}: {1}", item.Key.name, item.Value.distance);
+                }
+            }
+        }
+
+        public int GetEdge(string from, string to)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetEdge(string from, string to)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- Graphs/Program.cs
+++ Graphs/Program.cs
@@ -10,6 +10,20 @@ namespace Graphs
     {
         static void Main(string[] args)
         {
+            Graph_AdjgrList graph = new Graph_AdjgrList();
+            graph.AddVertex("A");
+            graph.AddVertex("B");
+            graph.AddVertex("C");
+            graph.AddVertex("D");
+
+            graph.AddEdge("A", "B", 1);
+            graph.AddEdge("B", "C", 2);
+            graph.AddEdge("C", "D", 3);
+            graph.AddEdge("D", "A", 4);
+
+            graph.Print();
+
+            Console.ReadKey();
         }
     }
 }
